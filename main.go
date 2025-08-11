90398f30d1ba6ee3f10fe7db92f843f3778edf40
Nikita Yurlov <n.yurlov@smartway.today>
2025-08-11 13:30:55 +0300

    mvp

--- main.go
+++ main.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"time"
+	"net"
 
 	auth "github.com/frog-in-fog/proto-contracts/gen/auth"
 
 	"google.golang.org/grpc"
 )
 
+type authServer struct {
+	auth.UnimplementedAuthServiceServer
+}
+
+func (s *authServer) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
+	token := fmt.Sprintf("token-for-%s", req.Username)
+	return &auth.LoginResponse{Token: token}, nil
+}
+
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("failed to connect: %v", err)
-	}
-	defer conn.Close()
-
-	client := auth.NewAuthServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-
-	resp, err := client.Login(ctx, &auth.LoginRequest{
-		Username: "testuser",
-		Password: "1234",
-	})
-	if err != nil {
-		log.Fatalf("failed to login: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 
-	log.Printf("Received token: %s", resp.Token)
+	grpcServer := grpc.NewServer()
+	auth.RegisterAuthServiceServer(grpcServer, &authServer{})
+
+	log.Println("Auth service running on :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
